5bc70c2f0d7621b0100112f0cd11fe17e90c3de8
Prajwal <prajwalmashewadi.99@gmail.com>
2025-06-11 22:43:56 +0530

    Add files via upload
    updated

--- lab7.c
+++ lab7.c
@@ -1,58 +1,56 @@
+
 #include <stdio.h>
 
-int n, m, p[10], w[10];
-
-void greedy_knapsack() {
-    float profit = 0;
-    int capacity = m;
-
-    printf("Items included: ");
-
-    while (capacity > 0) {
-        float max_ratio = 0;
-        int idx = -1;
-
-        // Find item with max price/weight ratio
-        for (int i = 0; i < n; i++) {
-            if (w[i] != 0 && ((float)p[i] / w[i]) > max_ratio) {
-                max_ratio = (float)p[i] / w[i];
-                idx = i;
-            }
-        }
-
-        if (idx == -1) break; // No items left
-
-        if (w[idx] <= capacity) {
-            // Include whole item
-            printf("%d ", idx);
-            capacity -= w[idx];
-            profit += p[idx];
-            w[idx] = 0;  // Mark item as used
-        } else {
-            // Include fraction of item
-            float fraction = (float)capacity / w[idx];
-            printf("%d (%.2f portion)", idx, fraction);
-            profit += fraction * p[idx];
-            capacity = 0; // Knapsack full
-        }
-    }
-
-    printf("\nTotal profit = %.2f\n", profit);
-}
-
 int main() {
+    float weight[50], profit[50], ratio[50], Totalvalue = 0, temp, capacity;
+    int n, i, j;
+
     printf("Enter number of items: ");
     scanf("%d", &n);
 
-    printf("Enter weights: ");
-    for (int i = 0; i < n; i++) scanf("%d", &w[i]);
+    for (i = 0; i < n; i++) {
+        printf("Enter weight and profit for item[%d]:\n", i);
+        scanf("%f %f", &weight[i], &profit[i]);
+    }
 
-    printf("Enter prices: ");
-    for (int i = 0; i < n; i++) scanf("%d", &p[i]);
+    printf("Enter the capacity of knapsack:\n");
+    scanf("%f", &capacity);
 
-    printf("Enter knapsack capacity: ");
-    scanf("%d", &m);
+    for (i = 0; i < n; i++)
+        ratio[i] = profit[i] / weight[i];
 
-    greedy_knapsack();
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (ratio[i] < ratio[j]) {
+                temp = ratio[j];
+                ratio[j] = ratio[i];
+                ratio[i] = temp;
+
+                temp = weight[j];
+                weight[j] = weight[i];
+                weight[i] = temp;
+
+                temp = profit[j];
+                profit[j] = profit[i];
+                profit[i] = temp;
+            }
+        }
+    }
+
+    printf("Knapsack problem using Greedy Algorithm:\n");
+    
+    for (i = 0; i < n; i++) {
+        if (weight[i] > capacity)
+            break;
+        else {
+            Totalvalue += profit[i];
+            capacity -= weight[i];
+        }
+    }
+
+    if (i < n)
+        Totalvalue += ratio[i] * capacity;
+
+    printf("\nThe maximum value is: %f\n", Totalvalue);
     return 0;
 }
